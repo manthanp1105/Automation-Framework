ea5789263f5bd0b4065cf894dadb48dfb385c893
Manthan Pandya <mpandya1105@gmail.com>
2024-06-18 14:13:59 -0400

    files updated

--- src/test/java/com/qa/base/BaseTest.java
+++ src/test/java/com/qa/base/BaseTest.java
@@ -3,13 +3,12 @@ package com.qa.base;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import com.qa.factory.DriverManager;
 import com.qa.pages.AccountsPage;
 import com.qa.pages.LoginPage;
-import com.qa.pages.RegisterPage;
 
 public class BaseTest {
 
@@ -20,7 +19,7 @@ public class BaseTest {
 	//protected RegisterPage rp;
 	protected Properties prop;
 
-	@BeforeMethod
+	@BeforeTest
 	public void setUp() {
 
 		dm = new DriverManager();
@@ -34,7 +33,7 @@ public class BaseTest {
 
 	}
 
-	@AfterMethod
+	@AfterTest
 	public void tearDown() {
 		driver.quit();
 	}
